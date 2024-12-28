1981f940c6941498a0e2ecf95e841be4d70f70af
Jakub Sykora <sykora.jakub@centrum.cz>
2024-12-28 10:37:36 +0100

    Najdi velke a male pismeno

--- najdi_velke_pismeno.py
+++ najdi_velke_pismeno.py
@@ -1,7 +1,16 @@
-a = ["a", "L", "A", "K", "x", "Y", "z", "U", "v", "W"]
 
-for i in a:
-    if i.isupper():
-        print ("Je velke pismeno. ")
-    else:
-        print ("Je male pismeno")
+#def separate_letters(text):
+text = "Hello! I am Jakub. This is my python script to find UPPPER Letter and lower letter in a text"
+    
+upper_case = []
+lower_case = []
+
+
+for char in text:
+    if char.isupper():
+        upper_case.append(char)
+    elif char.islower():
+        lower_case.append(char)
+    
+print( upper_case) 
+print (lower_case)
